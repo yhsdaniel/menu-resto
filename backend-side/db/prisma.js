27d9e3159ed1f233b86bc93bc2c69ec438721ac8
yhsdaniel <daniel.kristi09@gmail.com>
2026-05-07 14:59:54 +0700

    update

--- backend-side/db/prisma.js
+++ backend-side/db/prisma.js
@@ -1,5 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
 
-const prisma = new PrismaClient()
+const connectionString = process.env.DATABASE_URL
+
+const pool = new pg.Pool({
+    connectionString,
+})
+
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({
+    adapter
+})
 
 export default prisma
--- backend-side/prisma.config.js
+++ backend-side/prisma.config.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "@prisma/config";
 
-console.log(process.env.DATABASE_URL);
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
   datasource: {
     url: process.env.DATABASE_URL,
-    shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL,
   },
 });
